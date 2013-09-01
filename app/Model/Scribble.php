5af5459f157fbe50687b638a4b46333eacfda20f
Yihang Ho <hoyihang5@gmail.com>
2013-09-01 20:02:12 +0800

    Add data validation.

--- app/Model/Scribble.php
+++ app/Model/Scribble.php
@@ -1,8 +1,14 @@
 <?php
 class Scribble extends AppModel {
+	public $validate = array(
+		"body" => array(
+			'rule'    => 'notEmpty',
+			'message' => 'Body cannot be left empty'
+			)
+		);
 	public function beforeSave($options = array()) {
 		if (!array_key_exists("ukey", $this->data["Scribble"]) || empty($this->data["Scribble"]["ukey"])) {
-			$this->data["Scribble"]["ukey"] = substr(md5(rand()), 0, 7);;
+			$this->data["Scribble"]["ukey"] = substr(md5(rand()), 0, 7);
 		}
 		if (empty($this->data["Scribble"]["title"])) {
 			$this->data["Scribble"]["title"] = "Untitled Scribble";
