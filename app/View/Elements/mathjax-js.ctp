44b9c96154983fb7cdb440a5eeebfc7d13d587dd
Yihang Ho <hoyihang5@gmail.com>
2013-09-03 21:14:19 +0800

    Include the latex-editor.

--- app/View/Elements/mathjax-js.ctp
+++ app/View/Elements/mathjax-js.ctp
@@ -1,4 +1,5 @@
 <?php
 echo $this->Html->script("http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML");
+echo $this->Html->script("jquery.caret");//Required for mathjax-preview to work
 echo $this->Html->script("mathjax-livepreview");
 ?>
--- app/webroot/js/mathjax-livepreview.js
+++ app/webroot/js/mathjax-livepreview.js
@@ -1,48 +1,73 @@
-var Preview = {
-	delay: 150,
-	preview: null,
-	buffer: null,
-	timeout: null,
-	mjRunning: false,
-	oldText: null,
+var Previewer = function(_textarea, _preview, _buffer, _buttons)
+{
+	this.textarea = _textarea;
+	this.preview = _preview;
+	this.buffer = _buffer;
+	this.buttons = _buttons;
 
-	Init: function () {
-		this.preview = $("div#ScribblePreview");
-		this.buffer = $("div#ScribbleBuffer");
-	},
+	this.delay = 150;
+	this.timeout = null;
+	this.mjRunning = false;
+	this.oldText = null;
+	this.cur_instance = this;
 
-	SwapBuffers: function () {
+	this.SwapBuffers = function() {
 		var buffer = this.preview, preview = this.buffer;
 		this.buffer = buffer; this.preview = preview;
-		buffer.hide();
-		preview.show();
-	},
+		this.buffer.hide();
+		this.preview.show();
+	};
 
-	Update: function () {
-		if (!this.preview) this.preview = $("div#ScribblePreview");
-		if (!this.buffer) this.buffer = $("div#ScribbleBuffer");
+	this.Update = function() {
 		if (this.timeout) {clearTimeout(this.timeout)}
-		this.timeout = setTimeout(this.callback,this.delay);
-	},
+		this.timeout = setTimeout(this.callback, this.delay);
+	};
 
-	CreatePreview: function () {
-		Preview.timeout = null;
+	this.CreatePreview = function() {
+		this.timeout = null;
 		if (this.mjRunning) return;
-		var text = document.getElementById("ScribbleInput").value;
-		if (text === this.oldtext) return;
+		var text = this.textarea.val();
+		if (text == this.oldtext) return;
 		this.buffer.html(this.oldtext = text);
 		this.mjRunning = true;
 		MathJax.Hub.Queue(
 			["Typeset",MathJax.Hub,this.buffer.get(0)],
 			["PreviewDone",this]
 		);
-	},
+	};
 
-	PreviewDone: function () {
+	this.PreviewDone = function() {
 		this.mjRunning = false;
 		this.SwapBuffers();
-	}
-};
+	};
 
-Preview.callback = MathJax.Callback(["CreatePreview",Preview]);
-Preview.callback.autoReset = true;
+	this.textarea.data("this", this.cur_instance);
+	this.textarea.keyup(function(){
+		var that = $(this).data("this");
+		that.Update.call(that);
+	});
+
+	this.buttons.data("this", this.cur_instance);
+	this.buttons.click(function(){
+		event.preventDefault();
+		var that = $(this).data("this");
+		if (!$(this).data("latex-code")) return;
+		that.textarea.insertAtCaret($(this).data("latex-code"));
+		if ($(this).data("caret-end-offset") !== undefined)
+			that.textarea.setCaretPosition(that.textarea.getCaretPosition()+$(this).data("caret-start-offset"), that.textarea.getCaretPosition()+$(this).data("caret-end-offset"));
+		else if ($(this).data("caret-start-offset") !== undefined)
+			that.textarea.setCaretPosition(that.textarea.getCaretPosition()+$(this).data("caret-start-offset"));
+		that.Update.call(that);
+	});
+}
+
+function Initialize(_textarea, _preview, _buffer, _buttons){
+	var output = new Previewer (_textarea, _preview, _buffer, _buttons);
+	output.callback = MathJax.Callback(["CreatePreview", output]);
+	output.callback.autoReset = true;
+	return output;
+}
+
+$(document).ready(function(){
+	var p = Initialize($("textarea#ScribbleInput"), $("div#ScribblePreview"), $("div#ScribbleBuffer"), $(".insert-latex"));
+});
