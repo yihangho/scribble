cfd5b7bd401487c2b817db2eea207a011a2b9c59
Yihang Ho <hoyihang5@gmail.com>
2013-09-09 15:14:30 +0800

    Use goo.gl API to shorten link.

--- app/Model/Scribble.php
+++ app/Model/Scribble.php
@@ -17,7 +17,21 @@ class Scribble extends AppModel {
 				// Fun fact: Exact probability is 36^(-8) = 3.54E-13
 				$this->data["Scribble"]["ukey"] = substr(md5(rand()), 0, 7);
 			} while($this->find('count', array("conditions" => array("ukey" => $this->data["Scribble"]["ukey"]))));
-			
+			$ukey = $this->data["Scribble"]["ukey"];
+		} else {
+			$ukey = $this->findById($this->data["Scribble"]["id"])["ukey"];
+		}
+
+		if (!array_key_exists("id", $this->data["Scribble"]) || empty($this->findById($this->data["Scribble"]["id"])["short_link"])) {
+			// TODO Include API key
+			$curl = curl_init("https://www.googleapis.com/urlshortener/v1/url");
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_HTTPHEADER, array("Content-Type: application/json"));
+			curl_setopt($curl, CURLOPT_POSTFIELDS, '{"longUrl": "http://'.$_SERVER["SERVER_NAME"].'/'.$ukey.'"}');
+			$return = json_decode(curl_exec($curl), true);
+			$this->data["Scribble"]["short_link"] = $return["id"];
 		}
 		return true;
 	}
