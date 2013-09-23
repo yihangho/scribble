8774567e2030e949f5bb691093fa797320843218
Yihang Ho <hoyihang5@gmail.com>
2013-09-23 23:02:57 +0800

    Define the database schema in test.

--- app/Test/Fixture/ScribbleFixture.php
+++ app/Test/Fixture/ScribbleFixture.php
@@ -3,7 +3,20 @@ class ScribbleFixture extends CakeTestFixture {
 
 	public $useDbConfig = 'test';
 
-	public $import = 'Scribble';
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'ukey' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'short_link' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => true, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1),
+			'ukey' => array('column' => 'ukey', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB')
+	);
 
 	public $records = array(
 		array('id' => 1, 'title' => 'Test Scribble', 'body' => 'I am a Scribble!', 'ukey' => '1234567', 'short_link' => 'http://www.example.com', 'created' => '2013-10-12 01:01:01', 'modified' => '2013-10-12 01:01:01')
