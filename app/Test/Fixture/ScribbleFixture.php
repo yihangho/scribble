f044fd2c08716d1d5b6b6b4c82d358cc2f6edbbc
Yihang Ho <hoyihang5@gmail.com>
2013-09-26 20:50:12 +0800

    Update Scribble fixture to reflect the change in the database schema.

--- app/Test/Fixture/ScribbleFixture.php
+++ app/Test/Fixture/ScribbleFixture.php
@@ -5,6 +5,7 @@ class ScribbleFixture extends CakeTestFixture {
 
 	public $fields = array(
 		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'key' => 'primary'),
+		'user_id' => array('type' => 'integer', 'null' => true, 'default' => null),
 		'title' => array('type' => 'string', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'body' => array('type' => 'text', 'null' => true, 'default' => null, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
 		'ukey' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'unique', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
@@ -19,6 +20,6 @@ class ScribbleFixture extends CakeTestFixture {
 	);
 
 	public $records = array(
-		array('id' => 1, 'title' => 'Test Scribble', 'body' => 'I am a Scribble!', 'ukey' => '1234567', 'short_link' => 'http://www.example.com', 'created' => '2013-10-12 01:01:01', 'modified' => '2013-10-12 01:01:01')
+		array('id' => 1, 'user_id' => null, 'title' => 'Test Scribble', 'body' => 'I am a Scribble!', 'ukey' => '1234567', 'short_link' => 'http://www.example.com', 'created' => '2013-10-12 01:01:01', 'modified' => '2013-10-12 01:01:01')
 	);
 }
